92c302cd9526fcaf4e15c8510f64bdb6d75f8445
Harriet Gilmour <90762936+harrietgilmour@users.noreply.github.com>
2023-06-12 14:04:38 +0000

    testing translate function

--- testing_functions.py
+++ testing_functions.py
@@ -1,10 +1,17 @@
 #import modules
+from functions import translate_df
 import re
 import pandas as pd
 import numpy as np
+import googletrans as gt
+from googletrans import Translator
+translator = Translator()
 
 
-data=pd.read_csv("/workspaces/codespaces-blank/oel_poland_working.csv")
+
+data=pd.read_csv("oel_poland_working.csv")
 
 print(data)
 
+#use translate_df function to translate the dataframe
+translated_df = translate_df(data)
